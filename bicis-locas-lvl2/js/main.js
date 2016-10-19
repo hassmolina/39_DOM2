bb7925eecc59c1e619e24bc32da1578f7ace8bc1
hassmolina <hassmolinagutierrez@gmail.com>
2016-10-19 14:53:13 -0500

    ejercicio

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,25 +1,89 @@
-function validateForm(){
-	var name, lastname, email, password;
-	nombre = document.getElementById("name").value;
-	lastname = document.getElementById("lastname").value;
-	email = document.getElementById("input-email").value;
-	contrasena = document.getElementById("input-password").value;
+  function validateForm(){
+   	var nombre = document.getElementById("name").value;
+   	var apellido = document.getElementById("lastname").value;
+   	var email = document.getElementById("input-email").value;
+   	var contrasena = document.getElementById("input-password").value;
+   	var bici = document.querySelector("select").value;
+
+// NOMBRE
+
+   if(nombre.length == 0 || nombre == null || /^\s+$/.test(nombre)){//SI NO ESCRIBE NADA
+   	var span = document.createElement("span");//CREACION DE VARIABLE SUELTA EN JS
+   	var input = document.getElementById("name");//INPUT
+  	var div = input.parentNode;//DIV PADRE DEL INPUT
+   	div.insertBefore(span, input);//COLOCAR EL SPAN ANTES DEL HERMANO QUE ES EL INPUT
+   	var texto = document.createTextNode("Ingrese su nombre");//VARIABLE DE TEXTO
+   	span.appendChild(texto);//AGREGAR COMO HIJO "TEXTO" A SPAN CON APPENDCHILD
+   	return span;
+  } 
+  if (nombre.charAt(0) == nombre.charAt(0).toLowerCase()) {
+  			var span = document.createElement("span");
+  			var input = document.getElementById("name");
+  			var div = input.parentNode;
+  			div.insertBefore(span, input);
+	    	var texto = document.createTextNode("La primera letra debe ser Mayuscula");
+	    	span.appendChild(texto);
+	    	return span;
+	    }
 
 
-	// CAMPOS OBLIGATORIOS
-	if(span.nombre===""||lastname===""||email===""||contrasena===""){
-		alert("Todos los campos son obligatorios");
-		return false;
-	}
 
+// APELLIDO
 
+    if(apellido.length == 0 || apellido == null || /^\s+$/.test(apellido)){
+    	var span = document.createElement("span");
+    	var input = document.getElementById("lastname");
+    	var div = input.parentNode;
+    	div.insertBefore(span, input);
+    	var texto = document.createTextNode("Ingresa tu apellido");
+    	span.appendChild(texto);
+    	return span;
+    }
 
-	if(nombre == ""){
-		alert("El nombre esta vacio");
-		return false;
-	}else if(lastname==""){
-		alert("El apellido esta vacio");
-		return false;
-	}
+  if (apellido.charAt(0) == apellido.charAt(0).toLowerCase()) {
+  			var span = document.createElement("span");
+  			var input = document.getElementById("lastname");
+  			var div = input.parentNode;
+  			div.insertBefore(span, input);
+	    	var texto = document.createTextNode("La primera letra debe ser Mayuscula");
+	    	span.appendChild(texto);
+	    	return span;
+	    }
+  
+
+// CORREO
+
+var formula =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+if (!formula.test(email))
+ {
+ 	var span = document.createElement("span");
+  	var input = document.getElementById("input-email");
+   	var div = input.parentNode;
+  	div.insertBefore(span, input);
+ 	var texto = document.createTextNode("Verifica tu correo");
+ 	span.appendChild(texto);
+ 	return span;
+ }
+
+if(contrasena.length<=6 || contrasena == null || contrasena.length == 0 ||contrasena == "password" || contrasena == "123456" || contrasena == "098754" || /^\s+$/.test(contrasena)){
+	var span = document.createElement("span");
+  	var input = document.getElementById("input-password");
+   	var div = input.parentNode;		
+  	div.insertBefore(span, input);
+ 	var texto = document.createTextNode("Debe contener al menos 6 caracteres");
+ 	span.appendChild(texto);
+ 	return span;
+}
+
+    if(bici.selectedIndex == 0){
+    var span = document.createElement("span");
+  	var input = document.getElementByTagName("select")[0].selectedIndex;
+   	var div = input.parentNode;		
+  	div.insertBefore(span, input);
+ 	var texto = document.createTextNode("Debe contener al menos 6 caracteres");
+ 	span.appendChild(texto);
+ 	
+    }
 
 }
